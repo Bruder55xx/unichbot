3df3bc170506ea494c257799cf361696903226e8
Bruder <181657834+Bruder55xx@users.noreply.github.com>
2025-01-18 22:04:38 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,5 +1,8 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
+import http from 'http';
+
+export default function keep_alive() {
+  http.createServer((req, res) => {
+    res.write("I'm alive");
+    res.end();
+  }).listen(8080);
+}
